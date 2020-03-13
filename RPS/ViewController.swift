aee1be963d1b52c84b0d4dd5ad817ccd69deefac
kvsid <nice.curly2013@gmail.com>
2020-03-13 16:23:52 +0200

    Initial Commit

---
+++ RPS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RPS
+//
+//  Created by Xenia Sidorova on 13.03.2020.
+//  Copyright © 2020 Xenia Sidorova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
